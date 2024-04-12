93b90796ada1643b93009e514b9a875ad5ca5974
jufraas <jufracamachoposso@gmail.com>
2024-04-12 12:55:10 -0500

    cambios

--- src/components/Pautas.jsx
+++ src/components/Pautas.jsx
@@ -1,36 +1,71 @@
 import React, { useState } from "react";
 import "./Pautas.css";
+import pauta from "./imgs/pautas-img.png";
 
 const Pautas = () => {
-
   const handleInputChange = (event) => {
     console.log(event.target.value);
-  }
+  };
   return (
     <div className="contenedor">
       <div className="titulo">
         <h1>Inversion en Pautas</h1>
       </div>
-      <div className="opciones">
-        <div>
-          <h2>Vistas</h2>
-          <input type="text" className="vistas" onChange={handleInputChange}/>
-        </div>
-        <div>
-          <h2>Clicks</h2>
-          <input type="text" className="clicks" onChange={handleInputChange} />
-        </div>
-        <div>
-          <h2>Contactos</h2>
-          <input type="text" className="contactos" onChange={handleInputChange}/>
-        </div>
-        <div>
-          <h2>Ventas</h2>
-          <input type="text" className="Ventas" onChange={handleInputChange} />
-        </div>
-        <div>
-          <h2>Promedio</h2>
-          <input type="text" className="promedio"  onChange={handleInputChange}/>
+      <div className="contenedor2">
+        <img src={pauta} alt="" className="pauta-imagen" />
+        <div className="opciones">
+          <div>
+            <input
+              type="text"
+              className="inversion-pautas"
+              placeholder="5"
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              className="vistas"
+              placeholder="3"
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              className="clicks"
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              className="contactos"
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              className="Ventas"
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              className="promedio"
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <input
+              type="number"
+              className="ventas-generadas"
+              placeholder="35"
+              onChange={handleInputChange}
+            />
+          </div>
         </div>
       </div>
     </div>
